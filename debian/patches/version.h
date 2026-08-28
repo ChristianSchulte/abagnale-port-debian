28e3291bb9c41485b2a47e60af70842c82e712e5
Christian Schulte <cs@schulte.it>
2026-08-28 02:00:23 +0200

    Merged upstream tag v0/9699

--- debian/patches/version.h
+++ debian/patches/version.h
@@ -1,14 +1,14 @@
-Description: Adds build host information to HTTP User-Agent headers
+Description: Addes build host information to HTTP User-Agent headers
 Author: Christian Schulte <cs@schulte.it>
-Last-Update: 2026-08-22
+Last-Update: 2026-08-27
 
---- abagnale-0.9698.orig/version.h
-+++ abagnale-0.9698/version.h
+--- abagnale-0.9699.orig/version.h
++++ abagnale-0.9699/version.h
 @@ -17,6 +17,6 @@
  #ifndef VERSION_H
  #define VERSION_H
  
--#define ABAG_REVISION "$Abagnale: v0/9698 2026-08-25 23:53:41Z $"
-+#define ABAG_REVISION "Abagnale: v0/9698 2026-08-25 23:53:41Z; @DIST_NAME@ @ARCHITECTURE_NAME@ @PACKAGE@ @BUILD_HOST@ @BUILD_DATE@;"
+-#define ABAG_REVISION "$Abagnale: v0/9699 2026-08-27 23:33:41Z $"
++#define ABAG_REVISION "Abagnale: v0/9699 2026-08-27 23:33:41Z; @DIST_NAME@ @ARCHITECTURE_NAME@ @PACKAGE@ @BUILD_HOST@ @BUILD_DATE@;"
  
  #endif
